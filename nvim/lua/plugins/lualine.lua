9dbfa2ddfa3890032313ef969dcf62a081ab2a7d
Rain <Regniox@outlook.com>
2025-08-03 09:06:59 +0800

    remove lualine split symbol

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,7 +6,8 @@ return {
       icons_enabled = true,
       theme = "auto",
       section_separators = { left = "", right = "" },
-      component_separators = { left = "", right = "" },
+      component_separators = { left = "", right = "" },
+      -- component_separators = { left = "", right = "" },
       disabled_filetypes = {
         statusline = {},
         winbar = {},
